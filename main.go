fab832f47c3e97ce7e61f1b793d1577d96d42e2b
Bayathy <misosoup0321@icloud.com>
2021-12-16 15:33:48 +0900

    下呂の生成

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ import (
 )
 
 var (
-	width  = 600
-	height = 400
+	width  = 60
+	height = 40
 )
 
 type colorpicker struct {
@@ -27,62 +27,70 @@ type colorpicker struct {
 var (
 	red = colorpicker{
 		//#e2471d
+		color1: color.RGBA{226, 71, 29, 255},
 		//#e6140e
+		color2: color.RGBA{230, 20, 14, 255},
 		//#e66352
-
+		color3: color.RGBA{230, 99, 82, 255},
 	}
+
 	yellow = colorpicker{
 		//#f69e2c
+		color1: color.RGBA{246, 158, 44, 255},
 		//#dec443
+		color2: color.RGBA{222, 196, 67, 255},
 		//#d4aa6d
+		color3: color.RGBA{212, 170, 109, 255},
 	}
 	brown = colorpicker{
 		//#9a7b55
+		color1: color.RGBA{154, 123, 85, 255},
 		//#da8b40
+		color2: color.RGBA{218, 139, 64, 255},
 		//#db8468
+		color3: color.RGBA{208, 132, 104, 255},
 	}
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	img := image.NewNRGBA(image.Rect(0,0,width,height))
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
 	for x := 0; x < width; x++ {
-		for y := 0; y < height; y++{
+		for y := 0; y < height; y++ {
 			srand := rand.Intn(3)
-			switch rand.Intn(3){
+			switch rand.Intn(3) {
 			case 0:
-				switch srand{
-					case 0:
-						img.Set(x,y,red.color1)
-					case 1:
-						img.Set(x,y,red.color2)
-					case 2:
-						img.Set(x,y,red.color3)
-				}		
+				switch srand {
+				case 0:
+					img.Set(x, y, red.color1)
+				case 1:
+					img.Set(x, y, red.color2)
+				case 2:
+					img.Set(x, y, red.color3)
+				}
 			case 1:
-				switch srand{
-					case 0:
-						img.Set(x,y,yellow.color1)
-					case 1:
-						img.Set(x,y,red.color2)
-					case 2:
-						img.Set(x,y,red.color3)
+				switch srand {
+				case 0:
+					img.Set(x, y, yellow.color1)
+				case 1:
+					img.Set(x, y, red.color2)
+				case 2:
+					img.Set(x, y, red.color3)
 				}
 			case 2:
-				switch srand{
-					case 0:
-						img.Set(x,y,brown.color1)
-					case 1:
-						img.Set(x,y,brown.color2)
-					case 2:
-						img.Set(x,y,brown.color3)
+				switch srand {
+				case 0:
+					img.Set(x, y, brown.color1)
+				case 1:
+					img.Set(x, y, brown.color2)
+				case 2:
+					img.Set(x, y, brown.color3)
 				}
-			}		
+			}
 		}
 
-		
-		f, _ := os.Create("./image.png")
-		png.Encode(f,img)
+		f, _ := os.Create("./image2.png")
+		png.Encode(f, img)
 		f.Close()
 	}
 }
